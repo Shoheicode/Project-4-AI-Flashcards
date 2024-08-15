f18704ea4010b6b9119a0ed37071411f837599ee
ShoheiCode <ucodejasonirie@gmail.com>
2024-08-15 16:27:37 -0700

    updated payment layout

--- app/page.js
+++ app/page.js
@@ -95,40 +95,45 @@ export default function Home() {
         </Grid>
       </Box>
       <Box sx={{my: 6, textAlign: 'center'}}>
-        <Typography variant="h4" component="h2" gutterBottom>Pricing</Typography>
+        <Typography variant="h3" component="h2" gutterBottom>Pricing</Typography>
         <Grid container spacing={4} justifyContent="center">
           {/* Pricing plans */}
-          <Grid item xs = {12} md={4}>
-            <Box height={120} border={"2px solid black"} borderRadius={10}>
-            <Typography variant="h6"> Esay Text Input</Typography>
-            <Typography>
-              {' '}
-              Simply input your text and let our software do the rest. Creating
-              flashcards has never been easier
-            </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs = {12} md={4}>
-            <Box height={120} border={"2px solid black"} borderRadius={10}>
-              <Typography variant="h6"> Esay Access</Typography>
-              <Typography>
-              {' '}
-              Access on any device anywhere!
-              </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs = {12} md={4}>
-            <Box height={120} border={"2px solid black"} borderRadius={10}>
-              <Typography variant="h6"> Uses Artifical Intelligence</Typography>
+          <Grid item xs = {12} md={6}>
+            <Box height={180} border={"2px solid black"} borderRadius={10} bgcolor={'silver'}>
+              <Typography variant="h4">Silver</Typography>
+              <Typography variant="h5">$5 / month</Typography>
               <Typography>
                 {' '}
-                Artifical Intelligence is common now and it makes the complex tasks of
-                creating and manufacturing flashcards easy as 1,2,3
+                Access to basic flashcard features and limited storage
               </Typography>
+              <Button variant="contained" color="primary">Choose Bronze</Button>
             </Box>
           </Grid>
+
+          <Grid item xs = {12} md={6}>
+            <Box height={180} border={"2px solid black"} borderRadius={10} bgcolor={'gold'}>
+              <Typography variant="h4">Gold</Typography>
+              <Typography variant="h5">$10 / month</Typography>
+              <Typography>
+              {' '}
+              Access to more premium features like AI Specific help
+              
+              </Typography>
+              <Button variant="contained" color="primary">Choose Silver</Button>
+            </Box>
+          </Grid>
+
+          {/* <Grid item xs = {12} md={4}>
+            <Box height={180} border={"2px solid black"} borderRadius={10} bgcolor={'gold'}>
+              <Typography variant="h4">Gold</Typography>
+              <Typography variant="h5">$15 / month</Typography>
+              <Typography>
+                {' '}
+                you get a cool gold tag line and a bunch of other features
+              </Typography>
+              <Button variant="contained" color="primary">Choose Gold</Button>
+            </Box>
+          </Grid> */}
         </Grid>
       </Box>
       
