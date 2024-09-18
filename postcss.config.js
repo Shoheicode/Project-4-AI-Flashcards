a23333ad848089da7f2381ebe7b3016111031d67
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-18 16:58:44 -0700

    fixed the plugins. Should work now

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-    ],
-  }
+      'postcss-import',
+      'tailwindcss/nesting',
+      'tailwindcss',
+      'autoprefixer',
+  ]
+}
