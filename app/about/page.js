f452f7731abefe4ba56cc90325c0db1d514c49c6
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-20 19:07:32 -0700

    updated about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,11 @@
 "use client";
 import About from "@/components/AboutPage/about";
 import NavBar from "@/components/navbar/navbar";
+import { useUser } from "@clerk/nextjs";
 import { Box, Button, Stack, TextField } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useState, Fragment } from "react";
+const { isLoaded, isSignedIn, user } = useUser();
 
 export default function Home() {
   const router = useRouter();
