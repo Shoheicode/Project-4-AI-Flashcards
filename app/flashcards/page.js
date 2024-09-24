1a086789cfcaa8f938b8b1e0c2dacb37d92891f9
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-23 21:59:22 -0700

    updated and added font

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -89,6 +89,7 @@ export default function Flashcard() {
                 sx={{
                   color:"white",
                 }}
+                
               >
                 Cards
               </Typography>
