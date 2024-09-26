8c4c07371b2f3a6d60ac47c01df57a5d3bbc4d68
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-26 12:22:01 -0700

    updated page

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -99,99 +99,103 @@ export default function Flashcard() {
             }
           >
             <Stack
-              gap={4}
+              direction={"row"}
             >
-              <Typography variant="h1" className="startText" color={"white"}textAlign={"center"}>{search}</Typography>
-              <Divider/>
-            
-            {flashcards.length > 0 && (<Box>
-              <Stack gap={5}>
-                <Card>
-                    <CardActionArea onClick={() => handleCardClick(currentIndex)}>
-                      <CardContent>
-                        <Box sx={{ /* Styling for flip animation */ 
-                          perspective: '1000px',
-                          '& > div' :{
-                            transition: 'transform 0.6s',
-                            transformStyle: 'preserve-3d',
-                            position: 'relative',
-                            width: '100%',
-                            height: '200px',
-                            boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-                            transform: flipped[currentIndex] 
-                              ? 'rotateY(180deg)' 
-                              : 'rotate',
-                          },
-                          '& > div > div' :{
-                            position: 'absolute',
-                            width: '100%',
-                            height: '100%',
-                            backfaceVisibility: 'hidden',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            padding: 2,
-                            boxSizing:'border-box'
-                          },
-                          '& > div > div:nth-of-type(2)' :{
-                            transform: 'rotateY(180deg)',
-                          }
-                        }}>
-                        <div>
+              <Stack
+                gap={4}
+              >
+                <Typography variant="h1" className="startText" color={"white"}textAlign={"center"}>{search}</Typography>
+                <Divider/>
+              
+              {flashcards.length > 0 && (<Box>
+                <Stack gap={5}>
+                  <Card>
+                      <CardActionArea onClick={() => handleCardClick(currentIndex)}>
+                        <CardContent>
+                          <Box sx={{ /* Styling for flip animation */ 
+                            perspective: '1000px',
+                            '& > div' :{
+                              transition: 'transform 0.6s',
+                              transformStyle: 'preserve-3d',
+                              position: 'relative',
+                              width: '100%',
+                              height: '200px',
+                              boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+                              transform: flipped[currentIndex] 
+                                ? 'rotateY(180deg)' 
+                                : 'rotate',
+                            },
+                            '& > div > div' :{
+                              position: 'absolute',
+                              width: '100%',
+                              height: '100%',
+                              backfaceVisibility: 'hidden',
+                              display: 'flex',
+                              justifyContent: 'center',
+                              alignItems: 'center',
+                              padding: 2,
+                              boxSizing:'border-box'
+                            },
+                            '& > div > div:nth-of-type(2)' :{
+                              transform: 'rotateY(180deg)',
+                            }
+                          }}>
                           <div>
-                            <Typography variant="h5" component="div">
-                              {flashcards[currentIndex].front}
-                            </Typography>
+                            <div>
+                              <Typography variant="h5" component="div">
+                                {flashcards[currentIndex].front}
+                              </Typography>
+                            </div>
+                            <div>
+                              <Typography variant="h5" component="div">
+                                {flashcards[currentIndex].back}
+                              </Typography>
+                            </div>
                           </div>
-                          <div>
-                            <Typography variant="h5" component="div">
-                              {flashcards[currentIndex].back}
-                            </Typography>
-                          </div>
-                        </div>
-                        
-                      </Box>
-                    </CardContent> 
-                  </CardActionArea>
-                </Card>
-                <Box 
-                  width={"100%"}
-                  display={"flex"}
-                  justifyContent={"space-around"}
-                >
-                  <Button 
-                    variant='contained'
-                    onClick={goToPrevSlide}
-                    sx={
-                      {
-                        color: "black",
-                      bgcolor: "#23d5ab",
+                          
+                        </Box>
+                      </CardContent> 
+                    </CardActionArea>
+                  </Card>
+                  <Box 
+                    width={"100%"}
+                    display={"flex"}
+                    justifyContent={"space-around"}
+                  >
+                    <Button 
+                      variant='contained'
+                      onClick={goToPrevSlide}
+                      sx={
+                        {
+                          color: "black",
+                        bgcolor: "#23d5ab",
+                        }
                       }
-                    }
-                    className="buttonC"
-                  >
-                    Prev
-                  </Button>
-                  <Typography color={"white"}>
-                    {currentIndex + 1}/{flashcards.length}
-                  </Typography>
-                  <Button
-                    //style={rightArrowStyles}
-                    variant='contained'
-                    onClick={goToNextSlide}
-                    className="buttonC"
+                      className="buttonC"
+                    >
+                      Prev
+                    </Button>
+                    <Typography color={"white"}>
+                      {currentIndex + 1}/{flashcards.length}
+                    </Typography>
+                    <Button
+                      //style={rightArrowStyles}
+                      variant='contained'
+                      onClick={goToNextSlide}
+                      className="buttonC"
 
-                    sx={{
-                      color: "black",
-                      bgcolor: "#23d5ab",
-                    }}
-                  >
-                    Next
-                  </Button>
-                </Box>
+                      sx={{
+                        color: "black",
+                        bgcolor: "#23d5ab",
+                      }}
+                    >
+                      Next
+                    </Button>
+                  </Box>
+                </Stack>
+              </Box>
+              )}
               </Stack>
-            </Box>
-            )}
             </Stack>
           </Container>
         </Box>
