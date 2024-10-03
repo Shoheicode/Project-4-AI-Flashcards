8f84a34b76c93ad4eb067eecd1b49cea4f4fa74f
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-02 17:31:10 -0700

    nvm

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -38,10 +38,6 @@ export default function NavBar(){
 
     const [userName, setUsername] = React.useState("");
 
-    if(isLoaded == false){
-      return <></>
-    }
-
 
     const DrawerList = (
       <Box sx={{ width: 250,
