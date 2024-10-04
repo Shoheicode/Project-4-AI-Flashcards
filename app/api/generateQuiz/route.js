3664bcbc89d1100e6709f6c539e0daf92026824e
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-03 17:11:55 -0700

    changed the system prompt

--- app/api/generateQuiz/route.js
+++ app/api/generateQuiz/route.js
@@ -2,14 +2,17 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const systemPrompt = `You are a quiz creator. You take in text that contains an word along with a definition or answer
-and a multiple choice problem with the answer in it. Make sure to create exactly 10 questions for each of the responses you get.
+and a 4 choice multiple choice problem with the answer in it. Make sure to create exactly 10 questions for each of the responses you get.
 Both front and back should be one sentence long.
 You should return in the following JSON format:
 {
   "questions":[
     {
-      "front": "Front of the card",
-      "back": "Back of the card"
+      "title": "The work they are trying to guess"
+      "first answer": "third answer",
+      "second answer": "second answer",
+      "third answer": "third answer",
+      "correct answer": "the correct answer"
     }
   ]
 }
